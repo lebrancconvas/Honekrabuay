064a5ebde77cc6bfe730825927b39346532b8bf9
lebrancconvas <lebrancconvas@gmail.com>
2021-12-26 18:19:26 +0700

    🔨 Create POST Method for User Registration.

--- server/index.js
+++ server/index.js
@@ -4,10 +4,16 @@ const cors = require('cors');
 
 const port = process.env.PORT || 3002;
 
-app.use(cors());
+app.use(cors()); // Just use in development environment. 
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Test Server: Server is OK!');
+})
+
+app.post('/api/registeruser', (req, res) => {
+    console.log(req.body);
+    res.json({ status: 'Server is ok.' });
 })
 
 app.listen(port, () => {
