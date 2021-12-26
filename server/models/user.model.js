ee7c79f41e10d1b78890082bea4d0832a6dce3c0
lebrancconvas <lebrancconvas@gmail.com>
2021-12-26 18:54:03 +0700

    🐛 Fixing the undefined type.

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const User = new mongoose.Schema({
-    username: { type: string, required: true, unique: true },
-    email: { type: string, required: true, unique: true },
-    password: { type: string, required: true }
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
 }, { collection: 'user-data' });
 
 const model = mongoose.model('UserAuth', User);
