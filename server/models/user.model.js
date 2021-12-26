c37daba10b8034c7925eb3953bab97adb7911844
lebrancconvas <lebrancconvas@gmail.com>
2021-12-26 18:33:37 +0700

    🗃️ Update User Model.

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const User = new mongoose.Schema({
-    username: { type: string, required: true },
-    email: { type: string, required: true },
+    username: { type: string, required: true, unique: true },
+    email: { type: string, required: true, unique: true },
     password: { type: string, required: true }
 }, { collection: 'user-data' });
 
