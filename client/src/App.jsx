42b00a9f4427c6c70f6311b74a1456a5822bcde3
lebrancconvas <lebrancconvas@gmail.com>
2021-12-26 12:47:46 +0700

    💄 Create Homepage UI.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,20 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'; 
+import {Typography} from '@mui/material'; 
+import {Box} from '@mui/system';
+import Homepage from './pages/Homepage'; 
+
 function App() {
   return (
     <div> 
+        <Router>
+            <Box sx={{textAlign: 'center'}}>
+                <Typography> 
+                    <Routes>
+                        <Route exact path="/" element={<Homepage />} /> 
+                    </Routes>
+                </Typography>
+            </Box>
+        </Router>
     </div>
   );
 }
---
+++ client/src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+const Homepage = () => {
+	return (
+		<div>
+			<h1>Honekrabuay</h1> 
+		</div>
+	)
+}
+
+export default Homepage; 
