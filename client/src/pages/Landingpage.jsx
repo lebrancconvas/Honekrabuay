41b6dc30c2d21259a2bd57503918e1dbfe043395
lebrancconvas <lebrancconvas@gmail.com>
2021-12-26 19:12:42 +0700

    👔 Create Route from Landing Page to Registration Page and Login Page.

--- client/src/pages/Landingpage.jsx
+++ client/src/pages/Landingpage.jsx
@@ -1,13 +1,23 @@
 import React from 'react'; 
-import {Typography} from '@mui/material'; 
+import {useNavigate} from 'react-router-dom'; 
+import {Typography, Button} from '@mui/material'; 
 import {Box} from '@mui/system'; 
 
 const Landingpage = () => {
+	const navigator = useNavigate(); 
+	const toRegister = () => navigator('/register'); 
+	const toLogin = () => navigator('/login');  
 	return (
 		<div>
 			<Box>
 				<Typography variant="h3"> 
 					Honekrabuay 
+				</Typography>
+			</Box>
+			<Box mt={3}>
+				<Typography>
+					<Button variant="contained" sx={{mr: 5}} onClick={toRegister}>Get Started</Button>  
+					<Button variant="contained" onClick={toLogin}>Login</Button>  
 				</Typography>
 			</Box>
 		</div>
