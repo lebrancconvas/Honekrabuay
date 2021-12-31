5bd1afa2944860647be0e8d77645ac8780dbfa87
lebrancconvas <lebrancconvas@gmail.com>
2022-01-01 01:34:16 +0700

    🩹 Edit Footer.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Container, Link} from '@mui/material';
+import {Container, Typography, Link} from '@mui/material';
 import {Box} from '@mui/system';
 
 const Footer = () => {
@@ -8,7 +8,11 @@ const Footer = () => {
 			<footer>
 				<Box bgcolor="secondary" sx={{textAlign: 'center'}}>
 					<Container maxWidth="lg">
-						Made with <Link href="https://www.mongodb.com/mern-stack" target="_blank">MERN Stack</Link> by <Link href="https://github.com/lebrancconvas" target="_blank">Poom</Link> in 2021. 
+						<Typography>
+							<h2>
+								Made with <Link href="https://www.youtube.com/watch?v=7CqJlxBYj-M" target="_blank">MERN Stack</Link> by <Link href="https://github.com/lebrancconvas" target="_blank">Poom</Link> in 2021. 
+							</h2>
+						</Typography>
 					</Container>
 				</Box>
 			</footer>
