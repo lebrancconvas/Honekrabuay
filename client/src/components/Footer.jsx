aa5d4316b59bb38fc1ac42f0c76c15fe2149c97a
lebrancconvas <lebrancconvas@gmail.com>
2021-12-31 22:53:28 +0700

    💄 Create Footer.

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<div>
+			<footer>
+				Made with MERN Stack by Poom in 2021. 
+			</footer>
+		</div>
+	);
+};
+
+export default Footer;
