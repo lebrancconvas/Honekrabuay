6869499a6d4d5ba9044fa37085a2b4079596134e
lebrancconvas <lebrancconvas@gmail.com>
2021-12-31 22:58:11 +0700

    🗃️ Complete Issues Database.

--- server/models/issue.model.js
+++ server/models/issue.model.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
 const Issue = new mongoose.createSchema({
-    name,
-    address,
-    topic,
-    content
-});
+    name: { type: String, require: true },
+    address: { type: String, require: true },
+    topic: { type: String, require: true },
+    content: { type: String, require: true }
+}, { collection: 'issue-data' });
+
+const model = mongoose.model('UserIssue', Issue);
+
+module.export = model;
