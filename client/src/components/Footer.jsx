e502781abe785de703c5519a6539f76a00014335
lebrancconvas <lebrancconvas@gmail.com>
2021-12-31 23:54:12 +0700

    💄 Add Link.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import {Container, Link} from '@mui/material';
+import {Box} from '@mui/system';
 
 const Footer = () => {
 	return (
 		<div>
 			<footer>
-				Made with MERN Stack by Poom in 2021. 
+				<Box bgcolor="secondary" sx={{textAlign: 'center'}}>
+					<Container maxWidth="lg">
+						Made with <Link href="https://www.mongodb.com/mern-stack" target="_blank">MERN Stack</Link> by <Link href="https://github.com/lebrancconvas" target="_blank">Poom</Link> in 2021. 
+					</Container>
+				</Box>
 			</footer>
 		</div>
 	);
