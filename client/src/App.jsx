aebde605351a8e15e11174161cf68f04c2e29367
lebrancconvas <lebrancconvas@gmail.com>
2021-12-31 22:53:13 +0700

    🚚 Add IssueForm Route.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'; 
-import {Typography} from '@mui/material'; 
-import {Box} from '@mui/system';
+// import {Typography} from '@mui/material'; 
+// import {Box} from '@mui/system';
 import Landingpage from './pages/Landingpage'; 
 import Register from './pages/Register'; 
 import Login from './pages/Login'; 
+import IssueForm from './pages/content/IssueForm';
 import Navbar from './components/Navbar'; 
+import Footer from './components/Footer';
 
 
 function App() {
@@ -15,8 +17,10 @@ function App() {
             <Routes>
                 <Route exact path="/" element={<Landingpage />} /> 
                 <Route exact path="/register" element={<Register />} /> 
-                <Route exact path="/login" element={<Login />} />  
+                <Route exact path="/login" element={<Login />} /> 
+                <Route exact path="/issueform" element={<IssueForm />} /> 
             </Routes>
+            <Footer />
         </Router>
     </div>
   );
