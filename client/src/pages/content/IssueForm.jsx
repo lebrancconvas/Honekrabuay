fd91035aaa56b5ba409a874184a5071d1a750cd1
lebrancconvas <lebrancconvas@gmail.com>
2022-01-01 01:34:40 +0700

    💄 Issue Form Alignment Change.

--- client/src/pages/content/IssueForm.jsx
+++ client/src/pages/content/IssueForm.jsx
@@ -21,26 +21,26 @@ const IssueForm = () => {
 
 	return (
 		<div>
-			<Box sx={{textAlign: 'center'}}>
+			<Box sx={{textAlign: 'center'}} mt={10}>
 				<Box>
-					<Typography>
+					<Typography variant="h2">
 						Please Filled the information here. 
 					</Typography>
 				</Box>
 				<form onSubmit={handleIssue}>
-					<Box>
+					<Box mt={3}>
 						<TextField type="text" placeholder="Name" sx={{width: 500}} onChange={event => setName(event.target.value)} />
 					</Box>
-					<Box>
+					<Box mt={2}>
 						<TextField type="text" placeholder="Address" sx={{width: 500}} onChange={event => setAddress(event.target.value)} />
 					</Box>
-					<Box>
+					<Box mt={2}>
 						<TextField type="text" placeholder="Topic" sx={{width: 500}} onChange={event => setTopic(event.target.value)} />
 					</Box>
-					<Box>
+					<Box mt={2}>
 						<TextField type="text" placeholder="Content" sx={{width: 500}} onChange={event => setContent(event.target.value)} />
 					</Box>
-					<Box>
+					<Box mt={2} mb={27}>
 						<Button type="submit" variant="contained">
 							Submit
 						</Button>
