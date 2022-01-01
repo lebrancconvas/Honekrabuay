32e7b0b44f7ea767119a47bc7bfa14693903091a
lebrancconvas <lebrancconvas@gmail.com>
2022-01-01 20:27:32 +0700

    💄 Update Homepage UI.

--- client/src/pages/content/Homepage.jsx
+++ client/src/pages/content/Homepage.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
+import {Typography, Button} from '@mui/material';
+import {Box} from '@mui/system';
 
 const Homepage = () => {
+	const navigator = useNavigate();
+	const toRequest = navigator('/userissue');
 	return (
-		<div></div>
+		<div>
+			<Box>
+				<Typography>
+					ยินดีต้อนรับสู่หน้าเว็บไซต์ของโหนกระบวย
+				</Typography>
+			</Box>
+			<Box>
+				<Button variant="contained" onClick={toRequest}>
+					แจ้งเรื่องร้องเรียน
+				</Button>
+			</Box>
+		</div>
 	)
 }
 
